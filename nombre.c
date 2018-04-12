a469f93b48fe23caf8e897f7fc55d3ae0e217441
jcpmcode <jcpmcode@gmail.com>
2018-04-12 11:52:15 -0500

    se agrego nombre.c

---
+++ nombre.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+int main (void)
+{
+  char nombre[50] = "Jose Carlos";
+  int tam, i=0;
+
+
+  printf("%s\n", nombre);
+  tam = strlen(nombre);
+  for (i = tam-1; i>=0; i--)
+  {
+    printf("%c", nombre[i]);
+  }
+  printf("\n");
+}
